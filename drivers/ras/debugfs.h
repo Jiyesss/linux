cc58a80a7968e6a459e53c4f77ef412abb0949e7
Jiyesss <choijiyea7185@gmail.com>
2024-03-14 22:36:24 +0900

    first commit

--- drivers/ras/debugfs.h
+++
@@ -1,9 +0,0 @@
-/* SPDX-License-Identifier: GPL-2.0 */
-#ifndef __RAS_DEBUGFS_H__
-#define __RAS_DEBUGFS_H__
-
-#include <linux/debugfs.h>
-
-struct dentry *ras_get_debugfs_root(void);
-
-#endif /* __RAS_DEBUGFS_H__ */
